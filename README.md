2409fbf4ff1a64697422fd7a27e71ace1962adf4
andresegido <andresegidof@gmail.com>
2023-06-19 17:12:16 +0200

    First commit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # IdealGas
 
 [![Build Status](https://github.com/andresegido/IdealGas.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/andresegido/IdealGas.jl/actions/workflows/CI.yml?query=branch%3Amain)
+
+Small package to deal with ideal gas functions
+
+Also used by me as a small proyect to check how to build and use a Julia package.
--- src/IdealGas.jl
+++ src/IdealGas.jl
@@ -1,5 +1,28 @@
 module IdealGas
 
-# Write your package code here.
+const R = 8.314472
+
+# Ideal gas parameter functions
+Rg_mol(M) = R / M
+M(Rg) = R / Rg
+γ(CP, CV) = CP/CV
+Rg(CP, CV) = CP - CV
+CP(γ, Rg) = (γ/(γ-1)) * Rg
+CV(γ, Rg) = Rg / (γ-1)
+
+# Ideal gas (mol-based)
+P_mol(n, T, V) = n * R * T / V
+V_mol(n, T, P) = n * R * T / P
+T_mol(n, P, V) = P * V / (n * R)
+n_mol(P, V, T) = P * V / (R * T)
+
+# Ideal gas (density-based)
+T(P, ρ, Rg) = P / (Rg * ρ)
+P(ρ, T, Rg) = ρ * Rg * T
+ρ(P, T, Rg) = P / (Rg * T)
+
+μ(μ0, T, T0, cs) = μ0 * (T/T0) ^ (3/2) * (T0+cs)/(T+cs)
+
+sound(γ, Rg, T) = √(γ*Rg*T)
 
 end
