fd7beccdf4777220d9b2ca7bf82852769d548025
andresegido <andresegidof@gmail.com>
2023-07-10 20:09:49 +0200

    Declared argument Types. Defined functions γ, Rg, CP and CV to admit three possible arguments having to choose two.

--- src/IdealGas.jl
+++ src/IdealGas.jl
@@ -2,13 +2,65 @@ module IdealGas
 export R, Rg_mol, M, Rg, CP, CV, P_mol, V_mol, T_mol, n_mol, T, P, ρ, μ, sound
 const R = 8.314472
 
+# Union Type Real or Nothing
+RealOrNothing = Union{Real,Nothing}
+
 # Ideal gas parameter functions
-Rg_mol(M) = R / M
-M(Rg) = R / Rg
-γ(CP, CV) = CP/CV
-Rg(CP, CV) = CP - CV
-CP(γ, Rg) = (γ/(γ-1)) * Rg
-CV(γ, Rg) = Rg / (γ-1)
+
+Rg_mol(M::Real) = R / M
+M(Rg::Real) = R / Rg
+
+function γ(; CP::RealOrNothing=nothing, CV::RealOrNothing=nothing, Rg::RealOrNothing=nothing)
+    i = count(j->(j === nothing), [CP, CV, Rg])
+    i == 1 || error("From the available input options, 2 must be defined instead of $(i-1).")
+
+    if Rg === nothing
+        return CP / CV
+    elseif CV === nothing
+        return CP / (CP - Rg)
+    elseif CP === nothing
+        return (CV + Rg) / CV
+    end
+end
+
+function Rg(; CP::RealOrNothing=nothing, CV::RealOrNothing=nothing, γ::RealOrNothing=nothing)
+    i = count(j->(j === nothing), [CP, CV, γ])
+    i == 1 || error("From the available input options, 2 must be defined instead of $(3-i).")
+
+    if γ === nothing
+        return CP - CV
+    elseif CV === nothing
+        return CP * (1 - 1 / γ)
+    elseif CP === nothing
+        return CV * (γ - 1)
+    end
+end
+
+function CP(; γ::RealOrNothing=nothing, Rg::RealOrNothing=nothing, CV::RealOrNothing=nothing)
+    i = count(j->(j === nothing), [γ, Rg, CV])
+    i == 1 || error("From the available input options, 2 must be defined instead of $(3-i).")
+
+    if CV === nothing
+        return (γ / (γ - 1)) * Rg
+    elseif Rg === nothing
+        return γ * CV
+    elseif γ === nothing
+        return CV + Rg
+    end
+end
+
+function CV(; γ::RealOrNothing=nothing, Rg::RealOrNothing=nothing, CP::RealOrNothing=nothing)
+    i = count(j->(j === nothing), [γ, Rg, CP])
+    i == 1 || error("From the available input options, 2 must be defined instead of $(3-i).")
+
+    if CP === nothing
+        return Rg / (γ - 1)
+    elseif Rg === nothing
+        return CP / γ
+    elseif γ === nothing
+        return CP - Rg
+    end
+end
 
 # Ideal gas (mol-based)
 P_mol(n, T, V) = n * R * T / V
@@ -17,12 +69,12 @@ T_mol(n, P, V) = P * V / (n * R)
 n_mol(P, V, T) = P * V / (R * T)
 
 # Ideal gas (density-based)
-T(P, ρ, Rg) = P / (Rg * ρ)
-P(ρ, T, Rg) = ρ * Rg * T
-ρ(P, T, Rg) = P / (Rg * T)
+T(P::Real, ρ::Real, Rg::Real) = P / (Rg * ρ)
+P(ρ::Real, T::Real, Rg::Real) = ρ * Rg * T
+ρ(P::Real, T::Real, Rg::Real) = P / (Rg * T)
 
-μ(μ0, T, T0, cs) = μ0 * (T/T0) ^ (3/2) * (T0+cs)/(T+cs)
+μ(μ0::Real, T::Real, T0::Real, cs::Real) = μ0 * (T/T0) ^ (3/2) * (T0+cs)/(T+cs)
 
-sound(γ, Rg, T) = √(γ*Rg*T)
+sound(γ::Real, Rg::Real, T::Real) = √(γ*Rg*T)
 
 end
